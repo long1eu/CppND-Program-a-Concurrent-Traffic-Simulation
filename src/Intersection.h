02e304c164adab7c038009532f6b454fc0e741e6
Razvan Cristian Lung <lung.razvan@gmail.com>
2019-07-13 18:16:07 +0300

    add tasks

--- src/Intersection.h
+++ src/Intersection.h
@@ -6,54 +6,48 @@
 #include <mutex>
 #include <memory>
 #include "TrafficObject.h"
+#include "TrafficLight.h"
 
-// forward declarations to avoid include cycle
 class Street;
 class Vehicle;
 
-// auxiliary class to queue and dequeue waiting vehicles in a thread-safe manner
-class WaitingVehicles
-{
-public:
-    // getters / setters
-    int getSize();
+class WaitingVehicles {
+ public:
+  int getSize();
 
-    // typical behaviour methods
-    void pushBack(std::shared_ptr<Vehicle> vehicle, std::promise<void> &&promise);
-    void permitEntryToFirstInQueue();
+  void pushBack(const std::shared_ptr<Vehicle>& vehicle, std::promise<void> &&promise);
+  void permitEntryToFirstInQueue();
 
-private:
-    std::vector<std::shared_ptr<Vehicle>> _vehicles;          // list of all vehicles waiting to enter this intersection
-    std::vector<std::promise<void>> _promises; // list of associated promises
-    std::mutex _mutex;
+ private:
+  std::vector<std::shared_ptr<Vehicle>> _vehicles;
+  std::vector<std::promise<void>> _promises;
+  std::mutex _mutex;
 };
 
-class Intersection : public TrafficObject
-{
-public:
-    // constructor / desctructor
-    Intersection();
+class Intersection : public TrafficObject {
+ public:
+  Intersection();
 
-    // getters / setters
-    void setIsBlocked(bool isBlocked);
+  void setIsBlocked(bool isBlocked);
 
-    // typical behaviour methods
-    void addVehicleToQueue(std::shared_ptr<Vehicle> vehicle);
-    void addStreet(std::shared_ptr<Street> street);
-    std::vector<std::shared_ptr<Street>> queryStreets(std::shared_ptr<Street> incoming); // return pointer to current list of all outgoing streets
-    void simulate();
-    void vehicleHasLeft(std::shared_ptr<Vehicle> vehicle);
-    bool trafficLightIsGreen();
+  void addVehicleToQueue(const std::shared_ptr<Vehicle>& vehicle);
+  void addStreet(const std::shared_ptr<Street>& street);
+  std::vector<std::shared_ptr<Street>> queryStreets(const std::shared_ptr<Street>& incoming);
+  void simulate();
+  void vehicleHasLeft(const std::shared_ptr<Vehicle>& vehicle);
+  bool trafficLightIsGreen();
 
-private:
+ private:
 
-    // typical behaviour methods
-    void processVehicleQueue();
+  // typical behaviour methods
+  void processVehicleQueue();
 
-    // private members
-    std::vector<std::shared_ptr<Street>> _streets;   // list of all streets connected to this intersection
-    WaitingVehicles _waitingVehicles; // list of all vehicles and their associated promises waiting to enter the intersection
-    bool _isBlocked;                  // flag indicating wether the intersection is blocked by a vehicle
+  // private members
+  std::vector<std::shared_ptr<Street>> _streets;
+  WaitingVehicles _waitingVehicles;
+  bool _isBlocked;
+
+  TrafficLight _trafficLight;
 };
 
 #endif
